ce6294fb1c76abaff659a026dad239c4549e492d
Rapha <oliver.raphaell@gmail.com>
2022-08-10 16:32:26 -0300

    inclui primeiros codigos do projeto do curso

---
+++ app/src/main/java/com/example/marvelapp/SplashActivity.kt
@@ -0,0 +1,18 @@
+package com.example.marvelapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.marvelapp.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+}
