162fcd329fbded622ab60a43a224e9b25aa4337c
Rapha <oliver.raphaell@gmail.com>
2022-09-16 19:02:23 -0300

    implementa module

---
+++ app/src/main/java/com/example/marvelapp/di/Module.kt
@@ -0,0 +1,40 @@
+package com.example.marvelapp.di
+
+import com.example.marvelapp.data.remote.ServiceApi
+import com.example.marvelapp.util.Constants.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object Module {
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(client: OkHttpClient) : Retrofit{
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideServiceAPi(retrofit: Retrofit): ServiceApi{
+        return retrofit.create(ServiceApi::class.java)
+    }
+}
